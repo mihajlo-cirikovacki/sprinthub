a9fafc1ba8830cc6471a431115e07f67b41a8fd0
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2023-03-08 21:44:06 +0100

    Added eslint rules and plugins and Husky pre-commit.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  env: {
+    browser: true,
+    es2022: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   overrides: [
     {
       extends: [
@@ -14,15 +25,57 @@ const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
+      },
+    ],
+
+    "func-style": ["error", "expression"],
+    "prefer-const": "error",
+    "no-console": "warn",
+
+    "react/button-has-type": "error",
+    "react/prop-types": "off",
+    "react/no-array-index-key": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-no-bind": "warn",
+
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off",
+
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
       },
     ],
   },
